7abdede3225ae01a1de16c7e4109256e8c0a8059
losandes <andes.collab@gmail.com>
2019-05-12 12:07:10 -0400

    Adds additional message to pre-push

--- pre-push.js
+++ pre-push.js
@@ -1,2 +1,3 @@
-const FORMAT = '\x1b[33m\x1b[5m%s\x1b[0m\n'
-console.log(FORMAT, 'Did you build the browser distribution?')
+const FORMAT = '\x1b[33m\x1b[5m%s\x1b[0m'
+console.log(FORMAT, 'Did you build the browser distribution?\n')
+console.log(FORMAT, 'Did you update the package number?')
