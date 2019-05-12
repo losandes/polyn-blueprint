8038e86c6ab2f30ea5748911830ea51aba086ffe
losandes <andes.collab@gmail.com>
2019-05-12 11:57:20 -0400

    Temporary Break to Test Pre-Push

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 const { expect } = require('chai')
 const suite = require('supposed')
   .Suite({ assertionLibrary: expect })
-
+process.exit(1)
 suite.sut = require('./index.js')
 suite.runner({
   directories: ['./src'],
