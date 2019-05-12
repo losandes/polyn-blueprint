b7205b1c5889352a6745b3bbb8b23d6917f5f6df
losandes <andes.collab@gmail.com>
2019-05-12 13:18:31 -0400

    Adds Cookbook to README

--- README.md
+++ README.md
@@ -17,6 +17,7 @@ If you use the `value` property that is returned from @polyn/blueprint's `valida
   * [Registering Blueprints](#registering-blueprints)
   * [Intercepting Values](#intercepting-values)
 * [TypeScript Support](#typescript-support)
+* [Cookbook](#cookbook)
 
 ## Usage
 
@@ -468,6 +469,72 @@ console.log(result.err)
 
 console.log(result.value)
 // prints { firstName: 'John', lastName: 'Doe', age: 21 }
+```
+
+## Cookbook
+
+* [Schema Inheritance](#schema-inheritance)
+
+### Schema Inheritance
+
+```JavaScript
+const { blueprint, registerBlueprint } = require('@polyn/blueprint')
+
+const productBp = blueprint('Product', {
+  id: 'string',
+  title: 'string',
+  description: 'string',
+  price: 'decimal:2',
+  type: /^book|magazine|card$/,
+  metadata: {
+    keywords: 'string[]'
+  }
+})
+
+registerBlueprint('Author', {
+  firstName: 'string',
+  lastName: 'string'
+})
+
+const bookBp = blueprint('Book', {
+  ...productBp.schema,
+  ...{
+    metadata: {
+      ...productBp.schema.metadata,
+      ...{
+        isbn: 'string',
+        authors: 'Author[]'
+      }
+    }
+  }
+})
+
+console.dir(productBp.validate({
+  id: '5623c1263b952eb796d79e02',
+  title: 'Happy Birthday',
+  description: 'A birthday card',
+  price: 9.99,
+  type: 'card',
+  metadata: {
+    keywords: ['bday']
+  }
+}), { depth: null })
+
+console.dir(bookBp.validate({
+  id: '5623c1263b952eb796d79e03',
+  title: 'Swamplandia',
+  description: 'From the celebrated...',
+  price: 9.99,
+  type: 'book',
+  metadata: {
+    keywords: ['swamp'],
+    isbn: '0-307-26399-1',
+    authors: [{
+      firstName: 'Karen',
+      lastName: 'Russell'
+    }]
+  }
+}), { depth: null })
 ```
 
 ## Why @polyn/blueprint? Why not JSON Schema
--- src/blueprint.test.js
+++ src/blueprint.test.js
@@ -841,6 +841,67 @@ module.exports = (test) => {
 
       expect(actual.err).to.not.be.null
       expect(actual.err.message).to.equal('Invalid sut: expected `string` {undefined} to be {string}')
+    },
+    'documentation': {
+      '// schema inheritance': () => {
+        // const { blueprint, registerBlueprint } = require('@polyn/blueprint')
+
+        const productBp = blueprint('Product', {
+          id: 'string',
+          title: 'string',
+          description: 'string',
+          price: 'decimal:2',
+          type: /^book|magazine|card$/,
+          metadata: {
+            keywords: 'string[]'
+          }
+        })
+
+        registerBlueprint('Author', {
+          firstName: 'string',
+          lastName: 'string'
+        })
+
+        const bookBp = blueprint('Book', {
+          ...productBp.schema,
+          ...{
+            metadata: {
+              ...productBp.schema.metadata,
+              ...{
+                isbn: 'string',
+                authors: 'Author[]'
+              }
+            }
+          }
+        })
+
+        console.dir(productBp.validate({
+          id: '5623c1263b952eb796d79e02',
+          title: 'Happy Birthday',
+          description: 'A birthday card',
+          price: 9.99,
+          type: 'card',
+          metadata: {
+            keywords: ['bday']
+          }
+        }), { depth: null })
+
+        console.dir(bookBp.validate({
+          id: '5623c1263b952eb796d79e03',
+          title: 'Swamplandia',
+          description: 'From the celebrated...',
+          price: 9.99,
+          type: 'book',
+          metadata: {
+            keywords: ['swamp'],
+            isbn: '0-307-26399-1',
+            authors: [{
+              firstName: 'Karen',
+              lastName: 'Russell'
+            }]
+          }
+        }), { depth: null })
+      }
     }
   })
 }
