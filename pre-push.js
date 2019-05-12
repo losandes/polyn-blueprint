973606a019a121b97395a4999cc972eff1e14407
losandes <andes.collab@gmail.com>
2019-05-12 12:05:58 -0400

    Updates pre-push formatting

--- pre-push.js
+++ pre-push.js
@@ -1,1 +1,2 @@
-console.log('\x1b[33m\x1b[5m%s\x1b[0m', 'Did you build the browser distribution?')
+const FORMAT = '\x1b[33m\x1b[5m%s\x1b[0m\n'
+console.log(FORMAT, 'Did you build the browser distribution?')
